f894a0a1d4e34d8de4d0bd2550c6323d48df8282
makarov alexey <mogrim90@gmail.com>
2016-09-23 16:56:02 +0300

    no states

--- sw.js
+++ sw.js
@@ -1,12 +1,5 @@
 'use strict';
 
-self.addEventListener('install', function(event) {
-    // self.skipWaiting();
-    console.log('Installed', event);
-});
-self.addEventListener('activate', function(event) {
-    console.log('Activated', event);
-});
 self.addEventListener('push', function(event) {
     console.log('Push message', event);
     fetch('localhost/api/push').then(function(response) {
